e309a69fbb6ddf5116b1141a50075ef8cc046d45
Brian Coutts <bjhcoutts@gmail.com>
2020-01-10 17:50:29 -0800

    next slide works, needs refactor

--- js/index.js
+++ js/index.js
@@ -4,7 +4,8 @@ const slides = Array.from(track.children)
 const prevButton = document.querySelector('.carousel__button--left') 
 const nextButton = document.querySelector('.carousel__button--right') 
 
-const carouselNav = document.querySelector('.carousel__nav');
+const carouselNav = document.querySelector('.carousel__nav')
+
 const dots = Array.from(carouselNav.children)
 
 const slideWidth = slides[0].getBoundingClientRect().width;
@@ -15,9 +16,22 @@ const setSlidePosition = (slide, index) => {
 
 slides.forEach(setSlidePosition);
 
+const moveToSlide = () => {
+	
+}
+
 nextButton.addEventListener('click', e => {
+	
 	const currentSlide = track.querySelector('.current-slide')
+
 	const nextSlide = currentSlide.nextElementSibling
+
 	const amountToMove = nextSlide.style.left
+	
 	track.style.transform = `translateX(-${amountToMove})`
+	
+	currentSlide.classList.remove('current-slide')
+	
+	nextSlide.classList.add('current-slide')
+
 })
